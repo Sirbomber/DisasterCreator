63b05d1b4c7dc150eaee0ed89cd1b21734acbad8
Sirbomber <47876550+Sirbomber@users.noreply.github.com>
2020-08-16 22:12:24 -0400

    Random disaster type bug fix

--- src/DisasterCreator.cpp
+++ src/DisasterCreator.cpp
@@ -59,11 +59,11 @@ void DisasterCreator::RollRandom()
 	{
 		target = trgRandom;
 	}
-	else if (rv < zoneWeight)
+	else if (rv < randWeight + zoneWeight)
 	{
 		target = trgZone;
 	}
-	else if (rv < plyrWeight)
+	else if (rv < randWeight + zoneWeight + plyrWeight)
 	{
 		target = trgPlayer;
 	}
